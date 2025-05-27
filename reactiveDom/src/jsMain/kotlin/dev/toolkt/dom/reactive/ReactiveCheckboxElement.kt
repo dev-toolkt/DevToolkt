76d3d8c0257ce5bb4fe128b25f1768477cb099ba
Jakub Trzebiatowski <cubuspl42@gmail.com>
2025-05-27 14:43:44 +0200

    Introduce `ReactiveSpanElement`

--- reactiveDom/src/jsMain/kotlin/dev/toolkt/dom/reactive/ReactiveCheckboxElement.kt
+++ reactiveDom/src/jsMain/kotlin/dev/toolkt/dom/reactive/ReactiveCheckboxElement.kt
@@ -3,6 +3,8 @@ package dev.toolkt.dom.reactive
 import dev.toolkt.dom.reactive.event.ReactiveCheckboxChangeEvent
 import dev.toolkt.dom.reactive.event.ReactiveEventHandler
 import dev.toolkt.dom.reactive.event.ReactiveMouseEvent
+import dev.toolkt.reactive.cell.Cell
+import dev.toolkt.reactive.event_stream.hold
 import dev.toolkt.reactive.reactive_list.ReactiveList
 
 class ReactiveCheckboxElement(
@@ -13,6 +15,16 @@ class ReactiveCheckboxElement(
     style = style,
     handleMouseDown = handleMouseDown,
 ) {
+    val isChecked: Cell<Boolean>
+        get() = onChange.map {
+            it.isChecked
+        }.hold(
+            initialValue = isCheckedNow,
+        )
+
+    val isCheckedNow: Boolean
+        get() = rawInputElement.checked
+
     override val changeEventWrapper = ReactiveCheckboxChangeEvent
 
     init {
