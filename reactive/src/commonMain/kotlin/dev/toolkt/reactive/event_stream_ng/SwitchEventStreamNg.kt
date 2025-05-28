bf8d7354c554dea175f24a2ea89ec2c478c178e7
Jakub Trzebiatowski <cubuspl42@gmail.com>
2025-05-28 12:53:43 +0200

    Extract `MultiplexingEventStreamNg`

--- reactive/src/commonMain/kotlin/dev/toolkt/reactive/event_stream_ng/SwitchEventStreamNg.kt
+++ reactive/src/commonMain/kotlin/dev/toolkt/reactive/event_stream_ng/SwitchEventStreamNg.kt
@@ -1,38 +1,19 @@
 package dev.toolkt.reactive.event_stream_ng
 
-import dev.toolkt.reactive.Subscription
 import dev.toolkt.reactive.cell_ng.CellNg
 
 class SwitchEventStreamNg<V>(
-    private val nestedCell: CellNg<CellNg<V>>,
-) : DependentEventStreamNg<V>() {
-    override fun observe(): Subscription = object : Subscription {
-        private val outerSubscription = nestedCell.newValues.listen { newInnerCell ->
-            notify(newInnerCell.currentValue)
+    nestedCell: CellNg<CellNg<V>>,
+) : MultiplexingEventStreamNg<CellNg<V>, V>() {
+    override val nestedObject: CellNg<CellNg<V>> = nestedCell
 
-            resubscribeToInner(newInnerCell = newInnerCell)
-        }
-
-        private var innerSubscription: Subscription = subscribeToInner(
-            innerCell = nestedCell.currentValue,
-        )
-
-        private fun subscribeToInner(
-            innerCell: CellNg<V>,
-        ): Subscription = innerCell.newValues.listen { event ->
-            notify(event)
-        }
-
-        private fun resubscribeToInner(
-            newInnerCell: CellNg<V>,
-        ) {
-            innerSubscription.cancel()
-            innerSubscription = subscribeToInner(innerCell = newInnerCell)
-        }
-
-        override fun cancel() {
-            innerSubscription.cancel()
-            outerSubscription.cancel()
-        }
+    override fun processNewInnerObject(
+        newInnerObject: CellNg<V>,
+    ) {
+        notify(newInnerObject.currentValue)
     }
+
+    override fun extractInnerStream(
+        innerObject: CellNg<V>,
+    ): EventStreamNg<V> = innerObject.newValues
 }
