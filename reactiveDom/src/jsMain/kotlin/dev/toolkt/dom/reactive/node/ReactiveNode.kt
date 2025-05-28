5d43f395439ec40e295ceff177aa8f18e4f2fa7d
Jakub Trzebiatowski <cubuspl42@gmail.com>
2025-05-28 19:11:46 +0200

    Nuke `dom.reactive.node`

--- reactiveDom/src/jsMain/kotlin/dev/toolkt/dom/reactive/node/ReactiveNode.kt
+++
@@ -1,7 +0,0 @@
-package dev.toolkt.dom.reactive.node
-
-import org.w3c.dom.Node
-
-abstract class ReactiveNode {
-    abstract val rawNode: Node
-}
--- reactiveDom/src/jsMain/kotlin/dev/toolkt/dom/reactive/node/element/ReactiveWrapperNode.kt
+++
@@ -1,8 +0,0 @@
-package dev.toolkt.dom.reactive.node.element
-
-import dev.toolkt.dom.reactive.node.ReactiveNode
-import org.w3c.dom.Node
-
-class ReactiveWrapperNode(
-    override val rawNode: Node,
-) : ReactiveNode()
