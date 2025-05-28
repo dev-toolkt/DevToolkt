16ac153e9800f6f151984dabf43557fd8d9c79e4
Jakub Trzebiatowski <cubuspl42@gmail.com>
2025-05-28 16:47:16 +0200

    `ManagedEventStreamNg`: Add weak listeners

--- reactive/src/commonMain/kotlin/dev/toolkt/reactive/event_stream_ng/ManagedEventStreamNg.kt
+++ reactive/src/commonMain/kotlin/dev/toolkt/reactive/event_stream_ng/ManagedEventStreamNg.kt
@@ -1,10 +1,14 @@
 package dev.toolkt.reactive.event_stream_ng
 
+import dev.toolkt.core.platform.PlatformWeakReference
+import dev.toolkt.core.platform.mutableWeakMapOf
 import dev.toolkt.reactive.Listener
 import dev.toolkt.reactive.Subscription
 
 abstract class ManagedEventStreamNg<E> : ActiveEventStreamNg<E>() {
     private val listeners = mutableSetOf<Listener<E>>()
+
+    private val weakListeners = mutableWeakMapOf<Any, WeakListener<Any, E>>()
 
     final override fun listen(
         listener: Listener<E>,
@@ -13,20 +17,65 @@ abstract class ManagedEventStreamNg<E> : ActiveEventStreamNg<E>() {
 
         if (!wasAdded) throw AssertionError("Listener is already registered")
 
+        potentiallyResume()
+
         return object : Subscription {
             override fun cancel() {
                 val wasRemoved = listeners.add(listener)
 
                 if (!wasRemoved) throw AssertionError("Listener wasn't registered")
+
+                potentiallyPause()
             }
         }
     }
 
-    final override fun <T> listenWeak(
+    final override fun <T : Any> listenWeak(
         target: T,
         listener: WeakListener<T, E>,
     ): Subscription {
-        TODO("Not yet implemented")
+        @Suppress("UNCHECKED_CAST")
+        val oldValue = weakListeners.put(
+            key = target,
+            value = listener as WeakListener<Any, E>,
+        )
+
+        if (oldValue != null) {
+            throw AssertionError("Weak listener for target $target is already registered")
+        }
+
+        val weakTargetReference = PlatformWeakReference(target)
+
+        potentiallyResume()
+
+        return object : Subscription {
+            override fun cancel() {
+                val target = weakTargetReference.get() ?: return
+
+                val previousValue = weakListeners.remove(target)
+
+                if (previousValue != listener) {
+                    throw AssertionError("Unexpected removed value")
+                }
+
+                potentiallyPause()
+            }
+        }
+    }
+
+    private val listenerCount: Int
+        get() = listeners.size + weakListeners.size
+
+    private fun potentiallyResume() {
+        if (listenerCount == 1) {
+            onResumed()
+        }
+    }
+
+    private fun potentiallyPause() {
+        if (listenerCount == 0) {
+            onPaused()
+        }
     }
 
     protected fun notify(
@@ -36,7 +85,16 @@ abstract class ManagedEventStreamNg<E> : ActiveEventStreamNg<E>() {
             it(event)
         }
 
-        // TODO: Notify weak listeners
+        if (weakListeners.isEmpty()) {
+            return
+        }
+
+        weakListeners.forEach { (target, weakListener) ->
+            weakListener(target, event)
+        }
+
+        // Iterating over the weak map may trigger unreachable entry purging
+        potentiallyPause()
     }
 
     protected abstract fun onResumed()
