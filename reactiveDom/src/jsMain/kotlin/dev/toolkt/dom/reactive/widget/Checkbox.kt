b90c7424d3d1ca32961a8860f1b07e0bec9f845e
Jakub Trzebiatowski <cubuspl42@gmail.com>
2025-05-28 19:29:29 +0200

    Implement `PureInputType`

--- reactiveDom/src/jsMain/kotlin/dev/toolkt/dom/reactive/widget/Checkbox.kt
+++ reactiveDom/src/jsMain/kotlin/dev/toolkt/dom/reactive/widget/Checkbox.kt
@@ -1,23 +1,26 @@
 package dev.toolkt.dom.reactive.widget
 
+import dev.toolkt.dom.pure.input.PureInputType
+import dev.toolkt.dom.reactive.utils.HTMLCheckboxElement
 import dev.toolkt.dom.reactive.utils.createReactiveHtmlInputElement
 import dev.toolkt.dom.reactive.utils.getChangeEventStream
 import dev.toolkt.dom.reactive.utils.getCheckedCell
 import dev.toolkt.reactive.cell.Cell
 import dev.toolkt.reactive.event_stream.EventStream
 import kotlinx.browser.document
-import org.w3c.dom.HTMLInputElement
 import org.w3c.dom.Node
 import org.w3c.dom.events.Event
 
 class Checkbox private constructor(
-    private val inputElement: HTMLInputElement,
+    private val inputElement: HTMLCheckboxElement,
 ) : Widget() {
     companion object {
         fun of(
             text: Cell<String>,
         ): Checkbox = Checkbox(
-            inputElement = document.createReactiveHtmlInputElement(type = "checkbox"),
+            inputElement = document.createReactiveHtmlInputElement(
+                type = PureInputType.Checkbox,
+            ),
         )
     }
 
