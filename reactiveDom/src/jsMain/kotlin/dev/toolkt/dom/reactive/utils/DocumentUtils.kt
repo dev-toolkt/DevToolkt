ea05077fd77582a7a4082d4c24e78b6a9bf90ec5
Jakub Trzebiatowski <cubuspl42@gmail.com>
2025-05-28 19:11:46 +0200

    Nuke `dom.reactive.node`

--- reactiveDom/src/jsMain/kotlin/dev/toolkt/dom/reactive/utils/DocumentUtils.kt
+++ reactiveDom/src/jsMain/kotlin/dev/toolkt/dom/reactive/utils/DocumentUtils.kt
@@ -1,22 +1,19 @@
 package dev.toolkt.dom.reactive.utils
 
-import dev.toolkt.dom.reactive.node.element.createReactiveHtmlElement
+import dev.toolkt.dom.pure.collections.childNodesList
 import dev.toolkt.dom.reactive.style.ReactiveStyle
 import dev.toolkt.reactive.cell.Cell
-import dev.toolkt.reactive.event_stream.EventStream
-import dev.toolkt.reactive.event_stream.cast
-import dev.toolkt.reactive.event_stream.getEventStream
 import dev.toolkt.reactive.reactive_list.ReactiveList
 import kotlinx.browser.document
 import org.w3c.dom.Document
+import org.w3c.dom.Element
 import org.w3c.dom.HTMLButtonElement
 import org.w3c.dom.HTMLDivElement
-import org.w3c.dom.HTMLElement
 import org.w3c.dom.HTMLInputElement
 import org.w3c.dom.HTMLSpanElement
 import org.w3c.dom.Node
 import org.w3c.dom.Text
-import org.w3c.dom.events.MouseEvent
+import org.w3c.dom.css.ElementCSSInlineStyle
 
 fun Document.createReactiveTextNode(
     data: Cell<String>,
@@ -30,6 +27,32 @@ fun Document.createReactiveTextNode(
         textNode.data = newValue
     },
 )
+
+fun Document.createReactiveHtmlElement(
+    /**
+     * A name of a styleable element
+     */
+    localName: String,
+    style: ReactiveStyle? = null,
+    children: ReactiveList<Node>? = null,
+): Element {
+    val element = this.createElement(
+        localName,
+    )
+
+    @Suppress("UNCHECKED_CAST_TO_EXTERNAL_INTERFACE") (element as ElementCSSInlineStyle)
+
+    style?.bind(
+        styleDeclaration = element.style,
+    )
+
+    children?.bind(
+        target = element,
+        extract = Node::childNodesList,
+    )
+
+    return element
+}
 
 fun Document.createReactiveHtmlInputElement(
     type: String,
@@ -46,6 +69,7 @@ fun Document.createReactiveHtmlInputElement(
 
     return inputElement
 }
+
 fun Document.createReactiveHtmlButtonElement(
     style: ReactiveStyle? = null,
     children: ReactiveList<Node>? = null,
--- reactiveDom/src/jsMain/kotlin/dev/toolkt/dom/reactive/widget/Widget.kt
+++ reactiveDom/src/jsMain/kotlin/dev/toolkt/dom/reactive/widget/Widget.kt
@@ -1,16 +1,10 @@
 package dev.toolkt.dom.reactive.widget
 
-import dev.toolkt.dom.reactive.node.element.ReactiveWrapperNode
 import dev.toolkt.reactive.reactive_list.ReactiveList
 import org.w3c.dom.Node
 
 abstract class Widget {
     abstract val rawNode: Node
-
-    val asReactiveElement: ReactiveWrapperNode
-        get() = ReactiveWrapperNode(
-            rawNode = rawNode,
-        )
 }
 
 val ReactiveList<Widget>.rawNodes: ReactiveList<Node>
