b996da308a3a5735c58e800791a8e084bc9aa713
Jakub Trzebiatowski <cubuspl42@gmail.com>
2025-05-28 13:13:30 +0200

    `ManagedEventStreamNg`: Manage strong listeners

--- reactive/src/commonMain/kotlin/dev/toolkt/reactive/event_stream_ng/ManagedEventStreamNg.kt
+++ reactive/src/commonMain/kotlin/dev/toolkt/reactive/event_stream_ng/ManagedEventStreamNg.kt
@@ -4,10 +4,22 @@ import dev.toolkt.reactive.Listener
 import dev.toolkt.reactive.Subscription
 
 abstract class ManagedEventStreamNg<E> : ActiveEventStreamNg<E>() {
+    private val listeners = mutableSetOf<Listener<E>>()
+
     final override fun listen(
         listener: Listener<E>,
     ): Subscription {
-        TODO("Not yet implemented")
+        val wasAdded = listeners.add(listener)
+
+        if (!wasAdded) throw AssertionError("Listener is already registered")
+
+        return object : Subscription {
+            override fun cancel() {
+                val wasRemoved = listeners.add(listener)
+
+                if (!wasRemoved) throw AssertionError("Listener wasn't registered")
+            }
+        }
     }
 
     final override fun <T> listenWeak(
@@ -20,7 +32,11 @@ abstract class ManagedEventStreamNg<E> : ActiveEventStreamNg<E>() {
     protected fun notify(
         event: E,
     ) {
-        TODO()
+        listeners.forEach {
+            it(event)
+        }
+
+        // TODO: Notify weak listeners
     }
 
     protected abstract fun onResumed()
