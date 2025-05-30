3688c5fff3b0827007de20a0aad71127453b85ad
Jakub Trzebiatowski <cubuspl42@gmail.com>
2025-05-30 10:33:38 +0200

    Add more HTML/SVG utils

--- reactiveDom/src/jsMain/kotlin/dev/toolkt/dom/reactive/utils/svg/SVGElementUtils.kt
+++ reactiveDom/src/jsMain/kotlin/dev/toolkt/dom/reactive/utils/svg/SVGElementUtils.kt
@@ -3,9 +3,12 @@ package dev.toolkt.dom.reactive.utils.svg
 import dev.toolkt.dom.pure.svg.PureSvg
 import dev.toolkt.dom.reactive.style.ReactiveStyle
 import dev.toolkt.dom.reactive.utils.createReactiveElement
+import dev.toolkt.geometry.Point
+import dev.toolkt.reactive.cell.Cell
 import dev.toolkt.reactive.reactive_list.ReactiveList
 import org.w3c.dom.Document
 import org.w3c.dom.Node
+import org.w3c.dom.svg.SVGAnimatedLength
 import org.w3c.dom.svg.SVGCircleElement
 import org.w3c.dom.svg.SVGElement
 import org.w3c.dom.svg.SVGSVGElement
@@ -21,8 +24,7 @@ fun Document.createReactiveSvgElement(
     children = children,
 ) as SVGElement
 
-
-fun Document.createReactiveSvgSvgCircleElement(
+fun Document.createReactiveSvgSvgElement(
     style: ReactiveStyle? = null,
     children: ReactiveList<SVGElement>? = null,
 ): SVGSVGElement = createReactiveSvgElement(
@@ -33,9 +35,59 @@ fun Document.createReactiveSvgSvgCircleElement(
 
 fun Document.createReactiveSvgCircleElement(
     style: ReactiveStyle? = null,
+    position: Cell<Point>,
+    radius: Double,
     children: ReactiveList<SVGElement>? = null,
-): SVGCircleElement = createReactiveSvgElement(
-    localSvgName = "circle",
-    style = style,
-    children = children,
-) as SVGCircleElement
+): SVGCircleElement {
+    val circleElement = createReactiveSvgElement(
+        localSvgName = "circle",
+        style = style,
+        children = children,
+    ) as SVGCircleElement
+
+    position.bind(
+        target = circleElement,
+        xAnimatedLength = circleElement.cx,
+        yAnimatedLength = circleElement.cy,
+    )
+
+    circleElement.r.baseValue = radius
+
+    return circleElement
+}
+
+var SVGAnimatedLength.baseValue: Double
+    get() = this.baseVal.value.toDouble()
+    set(value) {
+        this.baseVal.value = value.toFloat()
+    }
+
+private fun <T : Any> Cell<Point>.bind(
+    target: T,
+    xAnimatedLength: SVGAnimatedLength,
+    yAnimatedLength: SVGAnimatedLength,
+) {
+    this.bind(
+        target = target,
+        setX = { _, x -> xAnimatedLength.baseValue = x },
+        setY = { _, y -> yAnimatedLength.baseValue = y },
+    )
+}
+
+private fun <T : Any> Cell<Point>.bind(
+    target: T,
+    setX: (T, Double) -> Unit,
+    setY: (T, Double) -> Unit,
+) {
+    this.bindAndForget(
+        target = target,
+    ) { it, point ->
+        setX(it, point.x)
+    }
+
+    this.bindAndForget(
+        target = target,
+    ) { it, point ->
+        setY(it, point.y)
+    }
+}
