40d7158c0365ad85ceee842305fa7c2eca4178e8
Jakub Trzebiatowski <cubuspl42@gmail.com>
2025-06-04 13:17:32 +0200

    Present the mouse position using an SVG circle

--- reactiveDom/src/jsMain/kotlin/dev/toolkt/dom/reactive/utils/gestures/MouseOverGesture.kt
+++ reactiveDom/src/jsMain/kotlin/dev/toolkt/dom/reactive/utils/gestures/MouseOverGesture.kt
@@ -1,28 +1,33 @@
 package dev.toolkt.dom.reactive.utils.gestures
 
 import dev.toolkt.dom.reactive.utils.event.clientPoint
+import dev.toolkt.dom.reactive.utils.event.offsetPoint
 import dev.toolkt.dom.reactive.utils.html.getMouseEnterEventStream
 import dev.toolkt.dom.reactive.utils.html.getMouseLeaveEventStream
 import dev.toolkt.dom.reactive.utils.html.getMouseMoveEventStream
 import dev.toolkt.geometry.Point
 import dev.toolkt.reactive.cell.Cell
-import dev.toolkt.reactive.future.Future
 import dev.toolkt.reactive.event_stream.EventStream
 import dev.toolkt.reactive.event_stream.hold
+import dev.toolkt.reactive.future.Future
 import org.w3c.dom.Element
+import org.w3c.dom.events.MouseEvent
 
-data class MouseOverGesture(
-    val clientPosition: Cell<Point>,
+class MouseOverGesture(
+    private val newestMouseEvent: Cell<MouseEvent>,
     val onFinished: Future<Unit>,
-)
+) {
+    val clientPosition: Cell<Point>
+        get() = newestMouseEvent.map { it.clientPoint }
+
+    val offsetPosition: Cell<Point>
+        get() = newestMouseEvent.map { it.offsetPoint }
+}
 
 fun Element.onMouseOverGestureStarted(): EventStream<MouseOverGesture> =
     this.getMouseEnterEventStream().map { mouseEnterEvent ->
-        val initialClientPoint = mouseEnterEvent.clientPoint
-        val newClientPoints = getMouseMoveEventStream().map { it.clientPoint }
-
         MouseOverGesture(
-            clientPosition = newClientPoints.hold(initialClientPoint),
+            newestMouseEvent = getMouseMoveEventStream().hold(mouseEnterEvent),
             onFinished = getMouseLeaveEventStream().next().unit(),
         )
     }
