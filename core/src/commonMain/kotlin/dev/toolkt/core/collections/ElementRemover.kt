83c79c127015a8ad54239e6d9c391e3a64a62720
Jakub Trzebiatowski <cubuspl42@gmail.com>
2025-06-04 12:28:55 +0200

    Implement `insertEffectivelyWeak`

--- core/src/commonMain/kotlin/dev/toolkt/core/collections/ElementRemover.kt
+++ core/src/commonMain/kotlin/dev/toolkt/core/collections/ElementRemover.kt
@@ -1,5 +1,7 @@
 package dev.toolkt.core.collections
 
+import dev.toolkt.core.platform.PlatformWeakReference
+
 interface ElementRemover {
     /**
      * Removes the element from the collection.
@@ -40,6 +42,29 @@ fun <E> MutableCollection<E>.insert(element: E): ElementRemover? {
 }
 
 /**
+ * Inserts the element into the collection without keeping a strong reference
+ * to the inserted element.
+ *
+ * @return an [ElementRemover] that can be used to remove the element later
+ * (if the element was added), or `null` if the element was already present
+ * in the collection.
+ */
+fun <E : Any> MutableCollection<E>.insertWeak(element: E): ElementRemover? {
+    val wasAdded = this.add(element)
+
+    if (!wasAdded) return null
+
+    val elementWeakRef = PlatformWeakReference(element)
+
+    return object : ElementRemover {
+        override fun remove(): Boolean {
+            val element = elementWeakRef.get() ?: return false
+            return this@insertWeak.remove(element)
+        }
+    }
+}
+
+/**
  * Inserts the element into the collection, throwing an exception if it was
  * already present.
  *
@@ -52,3 +77,17 @@ fun <E> MutableCollection<E>.insertEffectively(
     element: E,
 ): ElementRemover =
     insert(element) ?: throw IllegalStateException("The collection already contains the element: $element")
+
+/**
+ * Inserts the element into the collection without keeping a strong reference
+ * to the inserted element, throwing an exception if it was already present.
+ *
+ * @return an [ElementRemover] that can be used to remove the element later.
+ *
+ * @throws IllegalStateException if the element was already present in the
+ * collection.
+ */
+fun <E : Any> MutableCollection<E>.insertEffectivelyWeak(
+    element: E,
+): ElementRemover =
+    insertWeak(element) ?: throw IllegalStateException("The collection already contains the element: $element")
