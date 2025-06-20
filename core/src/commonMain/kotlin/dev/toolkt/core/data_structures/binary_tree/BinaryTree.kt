7138e83bc6026cd787d1fd2fd461c5d40b86f9d7
Jakub Trzebiatowski <cubuspl42@gmail.com>
2025-06-20 13:01:19 +0200

    `BinaryTree`: Remove unused operations

--- core/src/commonMain/kotlin/dev/toolkt/core/data_structures/binary_tree/BinaryTree.kt
+++ core/src/commonMain/kotlin/dev/toolkt/core/data_structures/binary_tree/BinaryTree.kt
@@ -1,7 +1,5 @@
 package dev.toolkt.core.data_structures.binary_tree
 
-import kotlin.random.Random
-
 interface BinaryTree<out PayloadT, out ColorT> {
     /**
      * A side of the tree or the tree's node, either left or right.
@@ -236,25 +234,6 @@ fun <PayloadT, MetadataT> BinaryTree<PayloadT, MetadataT>.getRightChild(
     side = BinaryTree.Side.Right,
 )
 
-fun <PayloadT, MetadataT> BinaryTree<PayloadT, MetadataT>.calculateSubtreeSize(
-    subtreeRootHandle: BinaryTree.NodeHandle<PayloadT, MetadataT>,
-): Int {
-    val leftChild = getLeftChild(nodeHandle = subtreeRootHandle)
-    val rightChild = getRightChild(nodeHandle = subtreeRootHandle)
-
-    val leftSize = calculateSubtreeSizeOrZero(subtreeRootHandle = leftChild)
-    val rightSize = calculateSubtreeSizeOrZero(subtreeRootHandle = rightChild)
-
-    return leftSize + 1 + rightSize
-}
-
-fun <PayloadT, MetadataT> BinaryTree<PayloadT, MetadataT>.calculateSubtreeSizeOrZero(
-    subtreeRootHandle: BinaryTree.NodeHandle<PayloadT, MetadataT>?,
-): Int {
-    if (subtreeRootHandle == null) return 0
-    return calculateSubtreeSize(subtreeRootHandle = subtreeRootHandle)
-}
-
 /**
  * Get a relative location of the child on the given [side] of the node
  */
@@ -328,57 +307,6 @@ fun <PayloadT, MetadataT> BinaryTree<PayloadT, MetadataT>.locateRelatively(
     )
 }
 
-/**
- * @return the set of the given node and its proper ancestors
- */
-fun <PayloadT, MetadataT> BinaryTree<PayloadT, MetadataT>.getQuasiAncestors(
-    nodeHandle: BinaryTree.NodeHandle<PayloadT, MetadataT>,
-): Sequence<BinaryTree.NodeHandle<PayloadT, MetadataT>> = generateSequence(
-    seed = nodeHandle,
-) { currentNodeHandle ->
-    getParent(nodeHandle = currentNodeHandle)
-}
-
-fun <PayloadT, MetadataT> BinaryTree<PayloadT, MetadataT>.getAncestors(
-    nodeHandle: BinaryTree.NodeHandle<PayloadT, MetadataT>,
-): Sequence<BinaryTree.NodeHandle<PayloadT, MetadataT>> {
-    val parent = this.getParent(nodeHandle = nodeHandle) ?: return emptySequence()
-
-    return generateSequence(
-        seed = parent,
-    ) { currentNodeHandle ->
-        getParent(nodeHandle = currentNodeHandle)
-    }
-}
-
-fun <PayloadT, MetadataT> BinaryTree<PayloadT, MetadataT>.getChildCount(
-    nodeHandle: BinaryTree.NodeHandle<PayloadT, MetadataT>,
-): Int {
-    val leftChild = getChild(
-        nodeHandle = nodeHandle,
-        side = BinaryTree.Side.Left,
-    )
-
-    val rightChild = getChild(
-        nodeHandle = nodeHandle,
-        side = BinaryTree.Side.Right,
-    )
-
-    val l = if (leftChild != null) 1 else 0
-    val r = if (rightChild != null) 1 else 0
-    return l + r
-}
-
-fun <PayloadT, MetadataT> BinaryTree<PayloadT, MetadataT>.getSingleChild(
-    nodeHandle: BinaryTree.NodeHandle<PayloadT, MetadataT>,
-): BinaryTree.NodeHandle<PayloadT, MetadataT>? = getChild(
-    nodeHandle = nodeHandle,
-    side = BinaryTree.Side.Left,
-) ?: getChild(
-    nodeHandle = nodeHandle,
-    side = BinaryTree.Side.Right,
-)
-
 fun <PayloadT, MetadataT> BinaryTree<PayloadT, MetadataT>.getSideMostFreeLocation(
     side: BinaryTree.Side,
 ): BinaryTree.Location<PayloadT, MetadataT> {
@@ -407,46 +335,6 @@ fun <PayloadT, MetadataT> BinaryTree<PayloadT, MetadataT>.getSideMostFreeLocatio
         side = side,
     )
 }
-
-fun <PayloadT, MetadataT> BinaryTree<PayloadT, MetadataT>.getSideMostDescendant(
-    nodeHandle: BinaryTree.NodeHandle<PayloadT, MetadataT>,
-    side: BinaryTree.Side,
-): BinaryTree.NodeHandle<PayloadT, MetadataT> {
-    val sideChildHandle = getChild(
-        nodeHandle = nodeHandle,
-        side = side,
-    ) ?: return nodeHandle
-
-    return getSideMostDescendant(
-        nodeHandle = sideChildHandle,
-        side = side,
-    )
-}
-
-/**
- * Get the in-order neighbour (predecessor / successor) of the node associated with
- * [nodeHandle] on the specified [side].
- *
- * @return A handle to the in-order neighbour node, or null if the given node has
- * no child on the specified [side]. This neighbour node will have no children
- * on the side opposite to [side].
- */
-fun <PayloadT, MetadataT> BinaryTree<PayloadT, MetadataT>.getNextInOrderFreeLocation(
-    nodeHandle: BinaryTree.NodeHandle<PayloadT, MetadataT>,
-    side: BinaryTree.Side,
-): BinaryTree.RelativeLocation<PayloadT, MetadataT> {
-    val sideChildLocation = nodeHandle.getChildLocation(side = side)
-
-    val sideChildHandle = resolve(
-        location = sideChildLocation,
-    ) ?: return sideChildLocation
-
-    return getSideMostFreeLocation(
-        nodeHandle = sideChildHandle,
-        side = side.opposite,
-    )
-}
-
 fun <PayloadT, MetadataT> BinaryTree<PayloadT, MetadataT>.getInOrderPredecessor(
     nodeHandle: BinaryTree.NodeHandle<PayloadT, MetadataT>,
 ): BinaryTree.NodeHandle<PayloadT, MetadataT>? = getInOrderNeighbour(
@@ -457,44 +345,6 @@ fun <PayloadT, MetadataT> BinaryTree<PayloadT, MetadataT>.getInOrderPredecessor(
 fun <PayloadT, MetadataT> BinaryTree<PayloadT, MetadataT>.getInOrderSuccessor(
     nodeHandle: BinaryTree.NodeHandle<PayloadT, MetadataT>,
 ): BinaryTree.NodeHandle<PayloadT, MetadataT>? = getInOrderNeighbour(
-    nodeHandle = nodeHandle,
-    side = BinaryTree.Side.Right,
-)
-
-
-/**
- * Get the in-order descendant neighbour (predecessor / successor) of the node associated with
- * [nodeHandle] on the specified [side].
- *
- * @return A handle to the in-order neighbour node, or null if the given node has
- * no child on the specified [side]. This neighbour node will have no children
- * on the side opposite to [side].
- */
-fun <PayloadT, MetadataT> BinaryTree<PayloadT, MetadataT>.getInOrderDescendantNeighbour(
-    nodeHandle: BinaryTree.NodeHandle<PayloadT, MetadataT>,
-    side: BinaryTree.Side,
-): BinaryTree.NodeHandle<PayloadT, MetadataT>? {
-    val sideChild = getChild(
-        nodeHandle = nodeHandle,
-        side = side,
-    ) ?: return null
-
-    return getSideMostDescendant(
-        nodeHandle = sideChild,
-        side = side.opposite,
-    )
-}
-
-fun <PayloadT, MetadataT> BinaryTree<PayloadT, MetadataT>.getInOrderDescendantPredecessor(
-    nodeHandle: BinaryTree.NodeHandle<PayloadT, MetadataT>,
-): BinaryTree.NodeHandle<PayloadT, MetadataT>? = getInOrderDescendantNeighbour(
-    nodeHandle = nodeHandle,
-    side = BinaryTree.Side.Left,
-)
-
-fun <PayloadT, MetadataT> BinaryTree<PayloadT, MetadataT>.getInOrderDescendantSuccessor(
-    nodeHandle: BinaryTree.NodeHandle<PayloadT, MetadataT>,
-): BinaryTree.NodeHandle<PayloadT, MetadataT>? = getInOrderDescendantNeighbour(
     nodeHandle = nodeHandle,
     side = BinaryTree.Side.Right,
 )
